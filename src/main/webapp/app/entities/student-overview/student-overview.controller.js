4a56af4c6f417eb7c0262b4f39f98f2b25b29ff3
Danylo <goddan333@gmail.com>
2017-02-28 22:47:55 +0200

    fix a problem with the controller running

--- src/main/webapp/app/entities/student-overview/student-overview.controller.js
+++ src/main/webapp/app/entities/student-overview/student-overview.controller.js
@@ -5,9 +5,9 @@
         .module('kpicsApp')
         .controller('StudentOverviewController', StudentOverviewController);
 
-    StudentOverviewController.$inject = ['$scope', '$state', 'StudentOverview', 'ParseLinks', 'AlertService', 'paginationConstants', 'pagingParams'];
+    StudentOverviewController.$inject = ['$scope', '$state', 'ParseLinks', 'AlertService', 'paginationConstants', 'pagingParams'];
 
-    function StudentOverviewController ($scope, $state, StudentInfo, ParseLinks, AlertService, paginationConstants, pagingParams) {
+    function StudentOverviewController ($scope, $state, ParseLinks, AlertService, paginationConstants, pagingParams) {
         var vm = this;
 
         vm.loadPage = loadPage;
@@ -18,9 +18,10 @@
 
         loadAll();
 
+        vm.studentInfo = {};
         vm.studentInfo.id = 1000;
         vm.studentInfo.firstName = "Petro";
-        vm.studentInfo.lastName = "Bizeshenko";
+        vm.studentInfo.lastName = "Bizeshko";
         vm.studentInfo.email = "petro2000@ukr.net";
         vm.studentInfo.linkedin = "http:\\someLinkedIn";
         vm.studentInfo.github = "http:\\someGitHub";
@@ -31,11 +32,7 @@
         vm.studentInfo.about = "Some about info. Information technology (IT) is the application of computers to store, study, retrieve, transmit, and manipulate data, or information, often in the context of a business or other enterprise. IT is considered a subset of information and communications technology (ICT). In 2012, Zuppo proposed an ICT hierarchy where each hierarchy level \"contain some degree of commonality in that they are related to technologies that facilitate the transfer of information and various types of electronically mediated communications.\"";
 
         function loadAll () {
-            StudentInfo.query({
-                page: pagingParams.page - 1,
-                size: vm.itemsPerPage,
-                sort: sort()
-            }, onSuccess, onError);
+
             function sort() {
                 var result = [vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc')];
                 if (vm.predicate !== 'id') {
