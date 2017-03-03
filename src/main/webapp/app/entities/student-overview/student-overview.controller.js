a4a0c1c0d79a2cdae1ee3252eb7b0558aa623cdc
Danylo <goddan333@gmail.com>
2017-03-03 09:25:54 +0200

    Improved Student-Overview Page & add student info to controller

--- src/main/webapp/app/entities/student-overview/student-overview.controller.js
+++ src/main/webapp/app/entities/student-overview/student-overview.controller.js
@@ -31,6 +31,117 @@
         vm.studentInfo.group = "IT-32";
         vm.studentInfo.about = "Some about info. Information technology (IT) is the application of computers to store, study, retrieve, transmit, and manipulate data, or information, often in the context of a business or other enterprise. IT is considered a subset of information and communications technology (ICT). In 2012, Zuppo proposed an ICT hierarchy where each hierarchy level \"contain some degree of commonality in that they are related to technologies that facilitate the transfer of information and various types of electronically mediated communications.\"";
 
+        vm.description = "This course introduces the scientific principles behind food safety and sanitation practices as well as practical and effective methods you can implement in your plant to keep your products safe.";
+
+        vm.studentInfo.tracks = [
+            {
+                name: "C#",
+                mark: 85,
+                skills: [
+                    {
+                        name: "Level 1",
+                        description: ".Net Core. " + vm.description ,
+                        mark: 53
+                    },
+                    {
+                        name: "Level 2",
+                        description: "ADO. "+ vm.description ,
+                        mark: 21
+
+                    },
+                    {
+                        name: "Level 3",
+                        description: "ASP .NET." + vm.description,
+                        mark: 80
+                    }
+                ]
+            },
+            {
+                name: "JAVA",
+                mark: 67,
+                skills: [
+                    {
+                        name: "Level 1",
+                        description: "Java Core. " + vm.description ,
+                        mark: 43
+                    },
+                    {
+                        name: "Level 2",
+                        description: "JAVA EE. "+ vm.description ,
+                        mark: 82
+
+                    },
+                    {
+                        name: "Level 3",
+                        description: "SPRING MVC. " + vm.description,
+                        mark: 27
+                    }
+                ]
+            },
+            {
+                name:"JavaScript",
+                mark:58,
+                skills: [
+                    {
+                        name: "Level 1",
+                        description: "JS Core. " + vm.description ,
+                        mark: 53
+                    },
+                    {
+                        name: "Level 2",
+                        description: "ECMAScript. "+ vm.description ,
+                        mark: 72
+
+                    }
+                ]
+            },
+            {
+                name:"C++",
+                mark:42,
+                skills: [
+                    {
+                        name: "Level 1",
+                        description: "Core. " + vm.description ,
+                        mark: 78
+                    },
+                    {
+                        name: "Level 2",
+                        description: "Apps dev. "+ vm.description ,
+                        mark: 20
+
+                    },
+                    {
+                        name: "Level 3",
+                        description: "Apps dev pro. " + vm.description,
+                        mark: 55
+                    }
+                ]
+            },
+            {
+                name:"Assembler",
+                mark:8,
+                skills: [
+                    {
+                        name: "Level 1",
+                        description: "FLAGS. " + vm.description ,
+                        mark: 25
+                    },
+                    {
+                        name: "Level 2",
+                        description: "MOV|JUMP. "+ vm.description ,
+                        mark: 39
+
+                    },
+                    {
+                        name: "Level 3",
+                        description: "NE|NZ." + vm.description,
+                        mark: 17
+                    }
+                ]
+            }
+        ];
+
+
         function loadAll () {
 
             function sort() {
